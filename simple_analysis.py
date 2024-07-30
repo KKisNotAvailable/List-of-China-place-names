f315ecef3a0bcd201a79125474af13b8cace8a50
Kai Ke <ken19505@gmail.com>
2024-07-31 01:33:23 +0800

    almost done the first step analysis: drawing scatter plots for portcodes with higher frequencies to see if there's any time trend or anomaly.

--- simple_analysis.py
+++ simple_analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
 
 # check out https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
 pd.options.mode.copy_on_write = True
@@ -8,29 +10,75 @@ def analysis(df: pd.DataFrame):
     # ----------
     # Preporcess
     # ----------
-    # promote means the promotion year, so none year digits and NaN set to 0
-    df.fillna({'promote': 0}, inplace=True)
+    nan_to_zero = {
+        'promote': 0,
+        'begin': 0,
+        'transfer': 0,
+        'pay': 0
+    }
+
+    df.fillna(nan_to_zero, inplace=True)
+
+    # "promote" means the promotion year, 
+    # none year figures and NaN set to 0 for easier filter later
     df.loc[df['promote'] < 1800, "promote"] = 0
 
-    print(min(df[df['promote'] > 0]['promote']))
+    # "year" means the observation year, has 3 NaNs
+    df.dropna(subset=['year'], inplace=True, ignore_index=True)
+
+    # "begin" should be the year that worker started the job.
+    # so figures smaller than 1800 are set to 0 for easier filter later
+    df.loc[df['begin'] < 1800, "begin"] = 0
+    df.loc[:, 'begin'] = df['begin'].astype(int)
+
+    # "transfer" should mean the year of transferring to that recorded job
+    # not sure how this column will be used, simply changed the NaNs to 0
+    # for easier filter later
+
+    # "pay" is the wage, originally wants to igonre all the NaNs,
+    # but there are too many of them (15222), so I keep them for now
+    # NaN set to 0 for easier filter later
+
+    # --------
+    # Analysis
+    # --------
+    # filters for analysis
+    cond_c0 = df['certainty_lvl'] == 0 # 2274; 1: 859
+    cond_c2 = df['certainty_lvl'] == 2 # 6445
+    cond_c3 = df['certainty_lvl'] == 3 # 61351
+    cond_highpay = df['pay'] > 1000 # 22, one in shashi 1116, other in hankow
+
+    # portcode freq
+    conds = cond_c3 | cond_c2 # can apply other conds with '&' or '|'
+    port_freq = Counter(df.loc[conds, 'portcode'])
+    top_ports = port_freq.most_common(10)
+
+    print(top_ports)
+
+    # for scatter plot, ignore NaNs. 
+    # But need to report the total count and Na counts.
+    df_to_plot = df.loc[df['portcode'] == 195, ['year', 'pay']].reset_index(drop=True)
+
+
+    # https://ithelp.ithome.com.tw/articles/10211370
+    plt.scatter(
+        df_to_plot['year'].astype("int"),
+        df_to_plot['pay']
+    )
+
+    plt.savefig("books/test.jpg")
+    plt.close() 
+
     return
 
 def main():
     df = pd.read_excel("books/data_port_processed.xlsm", sheet_name="Sheet1")
 
-    print(df.shape)
-    print(df.columns)
-
     cols_to_keep = [
-        "year", "rank", "begin", "promote", "transfer",
-        "pay", "areacode", "portcode", "certainty_lvl",
-        "port", "possible_names"
+        "year", "rank", "begin", "promote", "transfer", "pay", "areacode", 
+        "portcode", "certainty_lvl", "port", "possible_names"
     ]
 
-    # I thought "year" should be the observation year
-    # and "begin" should be the year this guy started this job
-    # there are several records having year > begin
-    # TODO: find the several extent
     analysis(df[cols_to_keep])
 
 if __name__ == "__main__":
